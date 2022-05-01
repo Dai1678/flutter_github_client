ac9acb409b125c86e681c21f5c50652ab1d51d52
Dai1678 <dai7774@gmail.com>
2022-05-01 16:56:57 +0900

    create user_search_page layout

--- lib/ui/user_search/user_search_page.dart
+++ lib/ui/user_search/user_search_page.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class UserSearchPage extends HookConsumerWidget {
@@ -6,7 +7,56 @@ class UserSearchPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // TODO: implement build
-    throw UnimplementedError();
+    final _userNameTextEditingController =
+        useTextEditingController.fromValue(TextEditingValue.empty);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("ユーザー検索"),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Expanded(
+                    child: TextField(
+                  controller: _userNameTextEditingController,
+                  maxLines: 1,
+                  decoration: InputDecoration(
+                      labelText: "ユーザー名",
+                      border: const OutlineInputBorder(),
+                      suffixIcon: IconButton(
+                          onPressed: _userNameTextEditingController.clear,
+                          icon: const Icon(Icons.clear))),
+                )),
+                const SizedBox(width: 16.0),
+                ElevatedButton(
+                    onPressed: () {
+                      final userName = _userNameTextEditingController.text;
+                      print(userName);
+                    },
+                    child: const Text("検索"))
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                for (int i = 0; i < 30; i++)
+                  ListTile(
+                    leading: const Icon(Icons.supervised_user_circle_rounded),
+                    title: const Text("ユーザー"),
+                    onTap: () {
+                      print("click");
+                    },
+                  ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
