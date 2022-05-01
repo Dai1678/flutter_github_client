ec7d7808cff0d376af810afb073bc45eaa79c351
Dai1678 <dai7774@gmail.com>
2022-05-01 19:22:09 +0900

    add header Authorization

--- lib/data/remote/github_dio.dart
+++ lib/data/remote/github_dio.dart
@@ -1,6 +1,7 @@
 import 'package:dio/adapter.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_github_client/data/remote/secrets.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final githubDioProvider = Provider((_) => GithubDio());
@@ -10,6 +11,7 @@ class GithubDio with DioMixin implements Dio {
     options = BaseOptions(
       baseUrl: "https://api.github.com",
       contentType: "application/json",
+      headers: {"Authorization": "token $githubApiKey"},
       connectTimeout: 60000,
       sendTimeout: 60000,
       receiveTimeout: 60000,
