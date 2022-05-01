880132b83fdbb18e9b87d21f3e2ad7065259d3f4
Dai1678 <dai7774@gmail.com>
2022-05-01 20:49:39 +0900

    create search_repository.dart

---
+++ lib/data/repository/search_repository.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_github_client/data/model/user.dart';
+import 'package:flutter_github_client/data/remote/datasource/search_data_source.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final searchRepositoryProvider = Provider<SearchRepository>(
+    (ref) => SearchRepositoryImpl(ref.read(searchDataSourceProvider)));
+
+abstract class SearchRepository {
+  Future<List<User>> searchUserList(String searchWord, {int page = 1});
+}
+
+class SearchRepositoryImpl implements SearchRepository {
+  SearchRepositoryImpl(this._searchDataSource);
+
+  final SearchDataSource _searchDataSource;
+
+  @override
+  Future<List<User>> searchUserList(String searchWord, {int page = 1}) async {
+    return await _searchDataSource
+        .userList(searchWord, page)
+        .then((response) => response.items);
+  }
+}
