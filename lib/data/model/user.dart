b00d058cd4008ad381f5960ae6e2c0a6e2b691fd
Dai1678 <dai7774@gmail.com>
2022-05-02 17:54:08 +0900

    create user.dart

---
+++ lib/data/model/user.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user.freezed.dart';
+
+@freezed
+class User with _$User {
+  factory User({
+    required String userName,
+    required String fullName,
+    required String avatarUrl,
+    required int followers,
+    required int following,
+  }) = _User;
+}
