d16a14f24dba16afcfc1678d1d6dec60983034b0
Dai1678 <dai7774@gmail.com>
2022-05-02 16:57:20 +0900

    fix to use UserSearch in Ui Layer

--- lib/data/repository/search_repository.dart
+++ lib/data/repository/search_repository.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_github_client/data/model/user_search_body.dart';
+import 'package:flutter_github_client/data/model/user_search.dart';
 import 'package:flutter_github_client/data/remote/datasource/search_data_source.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -6,8 +6,7 @@ final searchRepositoryProvider = Provider<SearchRepository>(
     (ref) => SearchRepositoryImpl(ref.read(searchDataSourceProvider)));
 
 abstract class SearchRepository {
-  Future<List<UserSearchBody>> searchUserList(String searchWord,
-      {int page = 1});
+  Future<List<UserSearch>> searchUserList(String searchWord, {int page = 1});
 }
 
 class SearchRepositoryImpl implements SearchRepository {
@@ -16,10 +15,12 @@ class SearchRepositoryImpl implements SearchRepository {
   final SearchDataSource _searchDataSource;
 
   @override
-  Future<List<UserSearchBody>> searchUserList(String searchWord,
+  Future<List<UserSearch>> searchUserList(String searchWord,
       {int page = 1}) async {
-    return await _searchDataSource
-        .userList(searchWord, page)
-        .then((response) => response.items);
+    return await _searchDataSource.userList(searchWord, page).then((response) =>
+        response.items
+            .map((item) =>
+                UserSearch(userName: item.userName, avatarUrl: item.avatarUrl))
+            .toList());
   }
 }
